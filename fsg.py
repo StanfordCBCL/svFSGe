409ee4a121316c935def027a749fd1c2a59d6f3c
mrp089 <51269309+mrp089@users.noreply.github.com>
2022-11-10 11:07:16 -0800

    working aitken!!

--- fsg.py
+++ fsg.py
@@ -21,7 +21,7 @@ from matplotlib.ticker import MaxNLocator
 from vtk.util.numpy_support import vtk_to_numpy as v2n
 from vtk.util.numpy_support import numpy_to_vtk as n2v
 
-from svfsi import svFSI
+from svfsi import svFSI, sv_names
 
 if platform.system() == 'Darwin':
     usr = '/Users/pfaller/'
@@ -47,34 +47,26 @@ class FSG(svFSI):
 
     def run(self):
         # run simulation
-        self.main()
-
-        # plot convergence
         try:
-            self.plot_convergence()
-        except:
+            self.main()
+        except KeyboardInterrupt:
+            print('interrupted')
             pass
 
         # archive results
-        pdb.set_trace()
         self.archive()
+
+        # plot convergence
+        self.plot_convergence()
 
     def main(self):
         # print reynolds number
-        c1 = 2.0 * self.p['fluid']['rho'] * self.p['fluid']['q0']
-        c2 = self.mesh_p['r_inner'] * np.pi * self.p['fluid']['mu']
-        print('Re = ' + str(int(c1 / c2)))
-
-        # offset pressure to match homeostatic pressure in the middle of the domain
-        # self.poiseuille(0)
-        # p = self.curr.get(('fluid', 'press', 'vol'))
-        # self.p['p_offset'] = - (np.max(p) - np.min(p)) / 2.0
-        self.p['p_offset'] = 0.0
+        print('Re = ' + str(int(self.p['re'])))
 
         # loop load steps
         i = 0
         for t in range(self.p['nmax'] + 1):
-            print('=' * 40 + ' t ' + str(t) + ' ==== fp ' + '{:.2f}'.format(self.p_vec[t]) + ' ' + '=' * 40)
+            print('=' * 45 + ' t ' + str(t) + ' ==== fp ' + '{:.2f}'.format(self.p_vec[t]) + ' ' + '=' * 45)
 
             # loop sub-iterations
             for n in range(self.p['coup']['nmax']):
@@ -82,7 +74,7 @@ class FSG(svFSI):
                 i += 1
 
                 # perform coupling step
-                self.coup_step(i, t, n)
+                self.coup_step_fixed(i, t, n)
 
                 # check if simulation failed
                 for name, s in self.curr.sol.items():
@@ -92,20 +84,15 @@ class FSG(svFSI):
 
                 # get error
                 self.coup_err('solid', 'disp', i, t, n)
-                self.coup_err('fluid', 'wss', i, t, n)
-                self.coup_err('fluid', 'press', i, t, n)
+                # self.coup_err('fluid', 'wss', i, t, n)
+                # self.coup_err('fluid', 'press', i, t, n)
 
                 # screen output
-                out = 'i ' + str(i) + ' \tn ' + str(n)
+                out = 'i ' + str(i) + ' \tn ' + str(n) + '\t'
                 for name, e in self.err.items():
-                    out += '\t' + name + ' ' + '{:.2e}'.format(e[-1][-1])
-                # out += '\t\t'
-                # for name in self.err.keys():
-                #     if n == 0:
-                #         e_log = 0.0
-                #     else:
-                #         e_log = np.log10(self.err[name][-1][-1] / self.err[name][-1][-2])
-                #     out += '\t' + name + ' log ' + '{:.1f}'.format(e_log)
+                    out += '  ' + name + ' ' + '{:.2e}'.format(e[-1][-1])
+                for name, e in self.p['coup']['omega'].items():
+                    out += '  ' + name + ' ' + '{:.2e}'.format(e[-1][-1])
                 print(out)
 
                 # archive solution
@@ -131,28 +118,60 @@ class FSG(svFSI):
                 print('\tcoupling unconverged')
 
     def plot_convergence(self):
-        fields = ['disp', 'wss']
-        colors = ['b', 'r']
+        n_sol = len(self.err.keys())
+        col_err = 'k'
+        col_omg = 'r'
 
-        # fig, ax = plt.subplots(1, len(fields), figsize=(40, 10), dpi=200)
-        fig, ax1 = plt.subplots(figsize=(40, 10), dpi=200)
-        ax = [ax1, ax1.twinx()]
-        for i, (name, col) in enumerate(zip(fields, colors)):
-            ax[i].set_xlabel('sub-iteration $n$')
-            ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
-            ax[i].set_ylabel(name, color=col)
-            ax[i].tick_params(axis='y', colors=col)
-            ax[i].grid(True)
-            if name == 'r':
-                ax[i].set_yscale('log')
-            plot = []
-            for res in self.converged[name]:
-                for v in res:
-                    if 'disp' in name:
-                        plot += [np.mean(rad(v))]
-                    else:
-                        plot += [np.mean(v)]
-            ax[i].plot(plot, linestyle='-', color=col)  # , marker='o'
+        n_iter = [0]
+        fig, ax = plt.subplots(n_sol, 1, figsize=(40, 10), dpi=200, sharex='all', sharey='all')
+        for i, name in enumerate(self.err.keys()):
+            # get_axis handle
+            if n_sol == 1:
+                axi = ax
+            else:
+                axi = ax[i]
+                
+            # get iteration counts
+            if i == 0:
+                n_iter += [len(res) for res in self.err[name]]
+                n_iter = np.cumsum(n_iter)
+
+            # second axis for omega
+            ax2 = axi.twinx()
+
+            # collect results
+            for j, (res, omg) in enumerate(zip(self.err[name], self.p['coup']['omega'][name])):
+                # iteration numbers
+                x = np.arange(n_iter[j], n_iter[j + 1])
+
+                # plot error
+                axi.plot(x, res, linestyle='-', color=col_err)
+
+                # plot omega
+                ax2.plot(x, omg[:len(x)], color=col_omg)
+
+            # plot convergence criterion
+            axi.plot([0, n_iter[-1]], self.p['coup']['tol'] * np.ones(2), 'k--')
+
+            # axis settings
+            axi.tick_params(axis='y', colors=col_err)
+            axi.set_xticks(n_iter)
+            axi.set_xticks(np.arange(0, n_iter[-1]), minor=True)
+            axi.set_xlim([0, n_iter[-1]])
+            axi.set_ylabel('Residual ' + sv_names[name], color=col_err)
+            axi.set_yscale('log')
+            axi.set_ylim([1.0e-4, 1])
+            axi.grid(which='minor', alpha=0.2)
+            axi.grid(which='major', alpha=0.9)
+            if i == len(self.err.keys()) - 1:
+                axi.set_xlabel('Total iteration $i$')
+
+            ax2.tick_params(axis='y', colors=col_omg)
+            ax2.set_ylabel('Omega', color=col_omg)
+            ax2.set_ylim([-0.02, 1.02])
+            ax2.set_yticks(np.linspace(0, 1, 6))
+
+        # save to file
         fig.savefig(os.path.join(self.p['f_out'], 'convergence.png'), bbox_inches='tight')
         plt.show()
         plt.close(fig)
@@ -185,21 +204,25 @@ class FSG(svFSI):
         trg = os.path.join(self.p['f_out'], self.p['root'], 'FEMbeCmm.cpp')
         shutil.copyfile(src, trg)
 
-    def coup_step(self, i, t, n):
+    def coup_step_fixed(self, i, t, n):
         # store previous solutions
         self.bfor = self.prev.copy()
         self.prev = self.curr.copy()
 
         # step 1: fluid update
-        if self.p['fsi']:
-            if self.step('fluid', i, t):
-                return
+        if t > 0 and n == 0:
+            # replace solution with prediction
+            self.coup_predict(i, t, n)
         else:
-            self.poiseuille(t)
+            if self.p['fsi']:
+                if self.step('fluid', i, t):
+                    return
+            else:
+                self.poiseuille(t)
 
         # relax fluid update
-        self.coup_relax('fluid', 'press', i, t, n)
-        self.coup_relax('fluid', 'wss', i, t, n)
+        # self.coup_relax('fluid', 'press', i, t, n)
+        # self.coup_relax('fluid', 'wss', i, t, n)
 
         # step 2: solid update
         if self.step('solid', i, t):
@@ -265,8 +288,7 @@ class FSG(svFSI):
 
         # quadratically extrapolate from previous two load increments
         vec_m2 = self.converged[-3].get(kind)
-        # return 3.0 * vec_m0 - 3.0 * vec_m1 + vec_m2
-        return 2.5 * vec_m0 - 2.0 * vec_m1 + 0.5 * vec_m2
+        return 3.0 * vec_m0 - 3.0 * vec_m1 + vec_m2
 
     def predictor_tube(self, kind, t):
         d, f, p = kind
@@ -287,50 +309,28 @@ class FSG(svFSI):
                 return self.curr.get(kind)
 
     def coup_relax(self, domain, name, i, t, n):
+        # log interface solution for aitken relaxation
+        self.dk[name] += [deepcopy(self.curr.get((domain, name, 'int'))).flatten()]
+
+        # calculate new relaxation factor
+        self.coup_omega(name, i, t, n)
+
         # volume increment
         curr_v = deepcopy(self.curr.get((domain, name, 'vol')))
         prev_v = deepcopy(self.prev.get((domain, name, 'vol')))
-        bfor_v = deepcopy(self.bfor.get((domain, name, 'vol')))
 
-        # interface increment
-        curr_i = deepcopy(self.curr.get((domain, name, 'int')))
-        prev_i = deepcopy(self.prev.get((domain, name, 'int')))
-
-        # log increments for aitken relaxation
-        self.log[name] += [curr_i - prev_i]
-
-        # calculate new relaxation factor
-        omega = self.coup_omega(name, t)
-        self.p['coup']['omega'] = omega
-
-        if t == 0:
+        # relax update
+        if i == 1:
             vec_relax = curr_v
         else:
-            # vec_relax = 0.5 * curr_v + 0.375 * prev_v + 0.125 * bfor_v # best so far but starts oscillating at t=8
-            # vec_relax = 0.25 * curr_v + 0.625 * prev_v + 0.125 * bfor_v # more damping but more robust
-            # vec_relax = 0.5 * curr_v + 0.4 * prev_v + 0.1 * bfor_v # even better (but crashes at t=6)
-            # vec_relax = 0.5 * curr_v + 0.5 * prev_v # good but takes many iterations and oscillates
-            # vec_relax = 0.6 * curr_v + 0.3 * prev_v + 0.1 * bfor_v # oscillates
-
-            # if t == 6:
-            #     vec_relax = 0.25 * curr_v + 0.625 * prev_v + 0.125 * bfor_v
-            # elif t >= 7:
-            #     vec_relax = 0.125 * curr_v + 0.625 * prev_v + 0.25 * bfor_v
-            # else:
-            #     vec_relax = 0.5 * curr_v + 0.4 * prev_v + 0.1 * bfor_v
-
-            # vec_relax = 0.25 * curr_v + 0.625 * prev_v + 0.125 * bfor_v
-            # if t >= 5:
-            #     vec_relax = 0.125 * curr_v + 0.625 * prev_v + 0.25 * bfor_v
-
-            vec_relax = 0.5 * curr_v + 0.4 * prev_v + 0.1 * bfor_v
-            if t >= 2:
-                vec_relax = 0.25 * curr_v + 0.625 * prev_v + 0.125 * bfor_v
-            if t >= 4:
-                vec_relax = 0.125 * curr_v + 0.625 * prev_v + 0.25 * bfor_v
+            omega = self.p['coup']['omega'][name][-1][-1]
+            vec_relax = omega * curr_v + (1.0 - omega) * prev_v
 
         # update solution
         self.curr.add((domain, name, 'vol'), vec_relax)
+
+        # log interface solution for aitken relaxation
+        self.dtk[name] += [deepcopy(self.curr.get((domain, name, 'int'))).flatten()]
 
     def coup_err(self, domain, name, i, t, n):
         curri = deepcopy(self.curr.get((domain, name, 'int')))
@@ -339,7 +339,7 @@ class FSG(svFSI):
             err = 1.0
         else:
             # difference
-            diff = np.abs(self.log[name][-1])
+            diff = np.abs(self.dk[name][-1] - self.dtk[name][-2])
             if len(diff.shape) == 1:
                 diff_n = np.max(diff)
             else:
@@ -363,42 +363,35 @@ class FSG(svFSI):
         # append error norm
         self.err[name][-1].append(err)
 
-    def coup_omega(self, name, t):
+    def coup_omega(self, name, i, t, n):
         # no relaxation necessary during prestressing (prestress does not depend on wss)
         if t == 0:
-            return 1.0
+            omega = 1.0
+        else:
+            # get old relaxed solutions
+            dp = self.dk[name][-1]
+            dk = self.dk[name][-2]
 
-        # if name == 'disp':
-        #     return 0.25
-        # elif name == 'wss':
-        #     return 0.25
-        return 0.5
+            # get old unrelaxed solutions
+            dtk = self.dtk[name][-1]
+            dtm = self.dtk[name][-2]
 
-        if len(self.log[name]) < 2:
-            return self.p['coup']['omega0']
+            # aitken update
+            diff = dtk - dp - dtm + dk
+            omega = np.dot(dtk - dtm, diff) / np.dot(diff, diff)
 
-        # current and old solution
-        m1 = self.log[name][-1]
-        m2 = self.log[name][-2]
-        if len(m1.shape) == 2:
-            m1 = np.linalg.norm(m1, axis=1)
-            m2 = np.linalg.norm(m2, axis=1)
-        diff = m1 - m2
-        norm = np.linalg.norm(diff)
+            # lower bound
+            omega = np.max([omega, 0.1])
 
-        if norm == 0.0:
-            return self.p['coup']['omega0']
+            # upper bound
+            omega = np.min([omega, 1.0])
 
-        # aitken relaxation
-        omega = - self.p['coup']['omega'] * np.dot(m2, diff) / norm ** 2
+        # start a new sub-list for new load step
+        if n == 0:
+            self.p['coup']['omega'][name].append([])
 
-        # lower bound
-        omega = np.max([omega, 0.1])
-
-        # upper bound
-        omega = np.min([omega, 0.9])
-
-        return omega
+        # append
+        self.p['coup']['omega'][name][-1].append(omega)
 
 
 def rad(x):
