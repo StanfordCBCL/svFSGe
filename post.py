b994705d7c5e2542b915d0646a4614d028c77d5c
Martin R. Pfaller <pfaller@stanford.edu>
2023-08-16 14:24:20 -0700

    simplify locations

--- post.py
+++ post.py
@@ -23,7 +23,7 @@ def cra2xyz(cra):
     return np.array([cra[1] * np.sin(cra[0]), cra[1] * np.cos(cra[0]), cra[2]])
 
 def xyz2cra(xyz):
-    return np.array([np.arctan2(xyz[1], xyz[0]), np.sqrt(xyz[0]**2.0 + xyz[1]**2.0), xyz[2]])
+    return np.array([(np.arctan2(xyz[0], xyz[1]) + 2.0 * np.pi) % (2.0 * np.pi), np.sqrt(xyz[0]**2.0 + xyz[1]**2.0), xyz[2]])
 
 def read_config(json):
     # read simulation config
@@ -61,7 +61,7 @@ def get_ids(pts_xyz):
              6: np.pi,
              9: 1.5 * np.pi}
     
-    # radial coordinates of points to export:
+    # radial coordinates of points to export: inside, outside
     p_rad = {"out": ro,
              "in": ri}
 
@@ -69,53 +69,52 @@ def get_ids(pts_xyz):
     p_axi = {"start": 0.0,
              "mid": h / 2,
              "end": h}
-    
+
     # collect all point coordinates
-    points = {}
-    for an, ap in p_axi.items():
-        for cn, cp in p_cir.items():
-            for rn, rp in p_rad.items():
-                points[str(cn) + "_" + rn + "_" + an] = cra2xyz([cp, rp, ap])
-    
-    # collect all line coordinates
-    lines = {}
+    locations = {}
     for cn, cp in p_cir.items():
         for rn, rp in p_rad.items():
-            lines[str(cn) + "_" + rn] = cra2xyz([cp, rp, 0.0])
-    
+            for an, ap in p_axi.items():
+                identifiers = [(cn, rn, an), (":", rn, an), (cn, ":", an), (cn, rn, ":")]
+                for i in identifiers:
+                    locations[i] = [cp, rp, ap]
+
     # collect all point ids
-    pids = {}
-    for n, pt in points.items():
-        # get id
-        chk = [np.isclose(pts_xyz[:, i], pt[i]) for i in range(3)]
-        id = np.where(np.logical_and.reduce(np.array(chk)))[0]
-        assert len(id) == 1, "point definition not unique: " + str(pt)
-        pids[n] = id[0]
+    ids = {}
+    coords = {}
+    for loc, pt in locations.items():
+        chk = [np.isclose(pts_cra[:, i], pt[i]) for i in range(3) if loc[i] != ":"]
+        ids[loc] = np.where(np.logical_and.reduce(np.array(chk)))[0]
+        assert len(ids[loc]) > 0, "no points found: " + str(loc)
 
-    # collect all line ids
-    lids = {}
-    for n, ln in lines.items():
-        # get ids
-        chk = [np.isclose(pts_xyz[:, i], ln[i]) for i in range(2)]
-        id = np.where(np.logical_and.reduce(np.array(chk)))[0]
-        assert len(id) > 1, "line definition empty: " + str(ln)
+        # sort according to coordinate
+        if ":" in loc:
+            crd = pts_cra[ids[loc], list(loc).index(":")]
+            sort = np.argsort(crd)
+            ids[loc] = ids[loc][sort]
+            coords[loc] = crd[sort]
+            assert len(np.unique(crd)) == len(crd), "coordinates not unique: " + str(crd)
 
-        # sort according to z-coordinate
-        zs = pts_xyz[id, 2]
-        lids[n] = id[np.argsort(zs)]
-    return pids, lids
+    return ids, coords
 
-def get_results(results, pts, pids, lids):
-    # get displacements in radial coordinates at all extracted points
-    disp = defaultdict(list)
+def get_results(results, pts, ids):
+    # get post-processed quantities at all extracted locations
+    post = {}
+    for loc in ids.keys():
+        post[loc] = defaultdict(list)
+
+    # get results at all time steps
     for res in results:
-        extract_results(disp, results[0], res, pts, pids, "p")
-    extract_results(disp, results[0], results[-1], pts, lids, "l")
-    for loc in disp.keys():
-        disp[loc] = np.array(disp[loc])
-    return disp
+        extract_results(post, results[0], res, pts, ids)
 
-def extract_results(disp, res0, res, pts, ids, mode):
+    # convert to numpy arrays
+    for loc in post.keys():
+        for f in post[loc].keys():
+            post[loc][f] = np.squeeze(np.array(post[loc][f]))
+
+    return post
+
+def extract_results(post, res0, res, pts, ids):
     # get nodal displacements
     d = v2n(res.GetPointData().GetArray("Displacement"))
 
@@ -140,7 +139,7 @@ def extract_results(disp, res0, res, pts, ids, mode):
         rad = xyz2cra((pts + d).T)[1]
         rad0 = xyz2cra((pts).T)[1]
 
-        # Poiseuille estimated wss
+        # Poiseuille estimated wss (constants cancel out in stimulus function)
         wss = 4.0 * mu * q0 / np.pi / rad ** 3.0
         wss0 = 4.0 * mu * q0 / np.pi / rad0 ** 3.0
     
@@ -150,7 +149,7 @@ def extract_results(disp, res0, res, pts, ids, mode):
     stim_sig = trace / trace0 - 1.0
     stim_all = stim_sig / stim_wss
 
-    for n, pt in ids.items():
+    for loc, pt in ids.items():
         # displacement in polar coordinates
         diff = xyz2cra((pts[pt] + d[pt]).T) - xyz2cra(pts[pt].T)
 
@@ -160,33 +159,21 @@ def extract_results(disp, res0, res, pts, ids, mode):
         diff[0] -= np.pi
 
         # store displacements
-        disp[mode + "_disp_" + n] += [diff.T]
+        post[loc]["disp"] += [diff]
         
         # extract thickness and wss
-        if "out" in n:
-            n_in = n.replace("out", "in")
-            n_scalar = n.replace("_out", "")
-            d_out = pts[ids[n]] + d[ids[n]]
-            d_in = pts[ids[n_in]] + d[ids[n_in]]
-            if mode == "l":
-                disp["l_thick_" + n_scalar] = np.linalg.norm((d_out - d_in).T, axis=0)
-                disp["l_wss_" + n_scalar] = wss[pt]
-                disp["l_z_" + n_scalar] = pts[pt, 2]
-            else:
-                disp["p_thick_" + n_scalar] += [np.linalg.norm((d_out - d_in).T, axis=0)]
-                disp["p_wss_" + n_scalar] += [wss[pt]]
+        if loc[1] == "in":
+            dloc = (loc[0], "out", loc[2])
+
+            d1 = pts[ids[loc]] + d[ids[loc]]
+            d2 = pts[ids[dloc]] + d[ids[dloc]]
+
+            post[loc]["thick"] += [np.linalg.norm((d1 - d2).T, axis=0)]
+            post[loc]["wss"] += [wss[pt]]
         
         # extract stimuli
-        if mode == "l":
-            disp[mode + "_stim_all_" + n] = stim_all[pt]
-            disp[mode + "_stim_" + n] = np.array([stim_sig[pt], stim_wss[pt]]).T
-        else:
-            disp[mode + "_stim_all_" + n] += [stim_all[pt]]
-            disp[mode + "_stim_" + n] += [np.array([stim_sig[pt], stim_wss[pt]])]
-
-        # store z-coordinate for lines
-        if mode == "l":
-            disp["l_z_" + n] = pts[pt, 2]
+        post[loc]["stim_all"] += [stim_all[pt]]
+        post[loc]["stim"] += [np.array([stim_sig[pt], stim_wss[pt]])]
 
 def extract_scalar(scalar, res, pts, ids, mode):
     d = v2n(res.GetPointData().GetArray("Displacement"))
@@ -198,12 +185,15 @@ def extract_scalar(scalar, res, pts, ids, mode):
             d_in = pts[ids[m_in]] + d[ids[m_in]]
             scalar["p_" + m_thick] += [np.linalg.norm(d_out - d_in)]
 
-def post(f_out):
+def post_process(f_out):
     # check if FSGe or conventional G&R results
     if ".json" in f_out:
         fsge = True
         sim = read_config(f_out)
         fname = os.path.join(sim.p['f_out'], "partitioned", "converged", "tube_*.vtu")
+    elif "partitioned" in f_out:
+        fsge = True
+        fname = os.path.join(f_out, "tube_*.vtu")
     else:
         fsge = False
         fname = os.path.join(f_out, "gr_*.vtu")
@@ -215,33 +205,39 @@ def post(f_out):
     pts = v2n(res[0].GetPoints().GetData())
 
     # get point and line ids
-    pids, lids = get_ids(pts)
+    ids, coords = get_ids(pts)
 
     # extract displacements
-    return get_results(res, pts, pids, lids)
+    return get_results(res, pts, ids), coords
 
-def plot_disp(data, out, study):
-    # assemble all plots (quantity and location)
-    fields = ["thick", "wss"]
-    # stimuli = ["wss", "sig", "all"]
-    stimuli = ["", "_all"]
-    locations = ["start", "mid", "end"]
+def plot_disp(data, coords, out, study):
+    # plot locations
+    loc_cir = range(0, 12, 3)
+    loc_rad = ["in"] # "out"
+    loc_axi = ["mid"] # "start", "end"
 
-    for loc in locations:
-        plot_single(data, os.path.join(out, "disp_points_" + loc + ".png"), study, "p", "disp", "in_" + loc)
-        for s in stimuli:
-            plot_single(data, os.path.join(out, "stim" + s + "_points_" + loc + ".png"), study, "p", "stim" + s, "in_" + loc)
-        for res in fields:
-            plot_single(data, os.path.join(out, res + "_points_" + loc + ".png"), study, "p", res, loc)
-        
-    if study == "single":
-        plot_single(data, os.path.join(out, "disp_lines.png"), study, "l", "disp", "in")
-        for s in stimuli:
-            plot_single(data, os.path.join(out, "stim" + s + "_lines.png"), study, "l", "stim" + s, "in")
-        for res in fields:
-            plot_single(data, os.path.join(out, res + "_lines.png"), study, "l", res)
+    # plot all points
+    fields = ["disp", "thick", "wss", "stim", "stim_all"]
+    for lr in loc_rad:
+        for la in loc_axi:
+            for f in fields:
+                loc_points = []
+                loc_lines = []
 
-def plot_single(data, out, study, mode, quant, loc=""):
+                # loop circumferential locations
+                for lc in loc_cir:
+                    # plot single points
+                    loc_points += [(lc, lr, la)]
+
+                    # plot along axial lines
+                    loc_lines += [(lc, lr, ":")]
+                if study == "single":
+                    # plot circumferential ring
+                    plot_single(data, coords, out, study, f, [(":", lr, la)])
+                    plot_single(data, coords, out, study, f, loc_lines)
+                plot_single(data, coords, out, study, f, loc_points)
+
+def plot_single(data, coords, out, study, quant, locations):
     # plot text
     if quant == "disp":
         ylabel = ["Cir. displacement $\Delta\\theta$ [°]",
@@ -257,41 +253,17 @@ def plot_single(data, out, study, mode, quant, loc=""):
         ylabel = ["Intramular stimulus $\Delta\sigma_I$ [-]", "WSS stimulus $\Delta\\tau_w$ [-]"]
     else:
         raise RuntimeError("Unknown quantity: " + quant)
-    
-    xstudies = {"kski": "$K_{\\tau\sigma}$"}
-
-    # collect data for all parameter variations
-    if study != "single":
-        data_sorted = {}
-        params = sorted(data.keys())
-        sims = data[params[0]].keys()
-        for nam in sims:
-            data_sorted[nam] = {}
-            for j in range(0, 12, 3):
-                yres = "_".join(filter(None, [mode, quant, str(j), loc]))
-                data_sorted[nam][yres] = []
-                for k in params:
-                    data_sorted[nam][yres] += [data[k][nam][yres][-1]]
-                data_sorted[nam][yres] = np.array(data_sorted[nam][yres])
-        data = data_sorted
 
     # determine plot dimensions
     nx = len(data)
     ny = len(ylabel)
 
-    # plot properties
-    oclocks = range(0, 12, 3)
-    colors = [plt.cm.tab10(i) for i in [0, 1, 3, 2]]
-    # from matplotlib.colors import to_hex
-    # print([to_hex(c) for c in colors])
-    styles = ["-", "-", "-", ":"]
-    styles_cir = ["-", "-", ":", "-"]
-
     fig, ax = plt.subplots(ny, nx, figsize=(nx * 10, ny * 5), dpi=300, sharex="col", sharey="row")
     for j, (n, res) in enumerate(data.items()):
+        title = n.replace("&", "\&")
         for i in range(ny):
             # select plot position
-            if ny == 1:
+            if nx == 1 or ny == 1:
                 pos = j
             else:
                 pos = (i, j)
@@ -302,43 +274,80 @@ def plot_single(data, out, study, mode, quant, loc=""):
                 ax[pos].axhline(0, color='black', zorder=2)
 
             # loop mesh positions
-            for ik, k in enumerate(oclocks):
+            for ic, lc in enumerate(locations):
+                loc = list(lc)
+
                 # get data for y-axis
-                xres = "l_z_" +  "_".join(filter(None, [str(k), loc]))
-                yres =  "_".join(filter(None, [mode, quant, str(k), loc]))
-                title = n.replace("&", "\&")
-                ydata = res[yres]
-                assert len(ydata) > 0, "no data found: " + yres
+                ydata = res[lc][quant].copy()
+                if not len(ydata):
+                    return
+                if ":" in lc:
+                    ydata = ydata[-1].T
                 if ny > 1:
                     ydata = ydata.T[i]
-
+                    
                 # get data for x-axis
                 if study == "single":
-                    if mode == "p":
+                    if ":" in loc:
+                        xdata = coords[tuple(loc)].copy()
+                        dim = loc.index(":")
+                        if dim == 0:
+                            xlabel = "Vessel circumference [°]"
+                            xdata *= 180 / np.pi
+                            xticks = xdata[0::4].astype(int)
+                        elif dim == 1:
+                            xlabel = "Vessel radius [°]"
+                            xticks = xdata.astype(str)
+                        elif dim == 2:
+                            xlabel = "Vessel length [mm]"
+                            xticks = [0, 2, 4, 6, 7.5, 9, 11, 13, 15]
+                        loc.remove(":")
+                    else:
                         xdata = np.arange(0, len(ydata))
                         xlabel = "Load step [-]"
-                        xticks = np.arange(0, len(ydata), 2)
-                    elif mode == "l":
-                        xdata = res[xres]
-                        xlabel = "Vessel length [mm]"
-                        xticks = [0, 2, 4, 6, 7.5, 9, 11, 13, 15]
+                        xticks = xdata[::2]
+
+                    # plot properties
+                    if len(locations) > 1:
+                        colors = [plt.cm.tab10(i) for i in [0, 1, 3, 2]]
+                        styles = ["-", "-", "-", ":"]
+                        styles_cir = ["-", "-", ":", "-"]
+
+                        # file name
+                        fname = "_".join([quant] + [str(l) for l in loc[1:]]) + ".png"
                     else:
-                        raise RuntimeError("Unknown mode: " + mode)
-                else:
-                    xdata = params
-                    xlabel = xstudies[study]
+                        # plot properties
+                        styles = ["-"]
+                        styles_cir = styles
+                        colors = ["#8C1515"]
+
+                        # file name
+                        dim_names = ["cir", "rad", "axi"]
+                        fname = "_".join([quant, dim_names[dim]] + [str(l) for l in loc]) + ".png"
+                elif study == "kski":
+                    # get data for x-axis
+                    xdata = np.linspace(0.0, 1.0, 5)
+                    xlabel = "$K_{\\tau\sigma}$"
                     xticks = [0, 0.25, 0.5, 0.75, 1]
-                assert len(xdata) > 0, "no data found: " + xres
+
+                    # plot properties
+                    colors = [plt.cm.tab10(i) for i in [0, 1, 3, 2]]
+                    styles = ["-", "-", "-", ":"]
+                    styles_cir = ["-", "-", ":", "-"]
+                
+                    # file name
+                    fname = "_".join(["kski", quant] + [str(l) for l in loc[1:]]) + ".png"
+                else:
+                    raise ValueError("unknown study: " + study)
 
                 # convert to degrees
-                if quant == "disp" and i == 0:
-                    ydata *= - 180 / np.pi
-                    stl = styles_cir[ik]
+                if quant == "disp" and i == 0 and len(locations) > 1:
+                    stl = styles_cir[ic]
                 else:
-                    stl = styles[ik]
+                    stl = styles[ic]
 
                 # plot!
-                ax[pos].plot(xdata, ydata, stl, color=colors[ik], linewidth=2)
+                ax[pos].plot(xdata, ydata, stl, color=colors[ic], linewidth=2)
             ax[pos].set_xticks(xticks)
             ax[pos].set_xticklabels([str(x) for x in xticks])
             ax[pos].set_xlim([np.min(xdata), np.max(xdata)])
@@ -349,8 +358,9 @@ def plot_single(data, out, study, mode, quant, loc=""):
             if j == 0:
                 ax[pos].set_ylabel(ylabel[i])
     plt.tight_layout()
-    fig.savefig(out, bbox_inches='tight')
-    plt.close(fig)
+    fig.savefig(os.path.join(out, fname), bbox_inches='tight')
+    # plt.close(fig)
+    plt.cla()
 
 def plot_insult(out):
     lo = 15.0
@@ -393,11 +403,10 @@ def plot_insult(out):
         ax[i].set_xlabel(xlabel[i])
     plt.tight_layout()
     fig.savefig(os.path.join(out, "insult.png"), bbox_inches='tight')
-    plt.close(fig)
+    plt.clf()
 
 def main():
     # set study
-    # for kski in ["0.5"]:  
     for kski in np.linspace(0.0, 1.0, 5).astype(str):
         folder = "/Users/pfaller/work/repos/FSGe/study_aneurysm"
         geo = "coarse"
@@ -415,9 +424,9 @@ def main():
         # collect all results
         data = {}
         for n, o in inp.items():
-            data[n] = post(o)
+            data[n], coords = post_process(o)
         
-        plot_disp(data, out, "single")
+        plot_disp(data, coords, out, "single")
 
 def main_param():
     # set study
@@ -441,10 +450,51 @@ def main_param():
                "FSGe": os.path.join(fpath, "partitioned", "partitioned.json")}
         data[k] = {}
         for n, o in inp.items():
-            data[k][n] = post(o)
+            data[k][n], coords = post_process(o)
     
-    plot_disp(data, out, "kski")
+    # collect data for all parameter variations
+    data_sorted = {}
+    params = sorted(data.keys())
+    sims = data[params[0]].keys()
+    for nam in sims:
+        data_sorted[nam] = {}
+        for loc in data[params[0]][nam].keys():
+            data_sorted[nam][loc] = {}
+            for quant in data[params[0]][nam][loc].keys():
+                data_sorted[nam][loc][quant] = []
+
+                # append all parametric evaluations
+                for k in params:
+                    # extract last time step
+                    data_sorted[nam][loc][quant] += [data[k][nam][loc][quant][-1]]
+                data_sorted[nam][loc][quant] = np.array(data_sorted[nam][loc][quant])
+
+    plot_disp(data_sorted, coords, out, "kski")
+
+def main_arg(folder):
+    # define paths
+    out = os.path.join(folder, "post")
+    os.makedirs(out, exist_ok=True)
+
+    # post-process simulation (converged and unconverged)
+    data = {}
+    inp = {"FSGe": os.path.join(folder, "partitioned.json"),
+           "FSGe unvconverged": os.path.join(folder, "partitioned")}
+    
+    # collect all results
+    data = {}
+    for n, o in inp.items():
+        data[n], coords = post_process(o)
+    
+    plot_disp(data, coords, out, "single")
 
 if __name__ == "__main__":
-    main_param()
-    main()
+    parser = argparse.ArgumentParser(description="Post-process FSGe simulation")
+    parser.add_argument("out", nargs='?', default=None, help="svFSI output folder")
+    args = parser.parse_args()
+    if not args.out:
+        main_param()
+        main()
+    else:
+        main_arg(args.out)
+
